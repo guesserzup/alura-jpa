23e1261853c4324e58c516c4b4d417c85faeeae7
Gabriel Guesser <gabriel.guesser@zup.com.br>
2021-08-16 14:54:17 -0300

    created a class to test the cliente x conta relationship

---
+++ src/main/java/br/com/alura/jpa/testes/TestaRelacionamentoClienteConta.java
@@ -0,0 +1,32 @@
+package br.com.alura.jpa.testes;
+
+import br.com.alura.jpa.modelo.Cliente;
+import br.com.alura.jpa.modelo.Conta;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class TestaRelacionamentoClienteConta {
+    public static void main(String[] args) {
+
+        Conta conta = new Conta();
+        conta.setId(2L);
+
+        Cliente cliente = new Cliente();
+        cliente.setNome("Leonardo");
+        cliente.setEndereco("Rua do ouvidor, 50");
+        cliente.setProfissao("Professor");
+        cliente.setConta(conta);
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        em.persist(cliente);
+
+        em.getTransaction().commit();
+        em.close();
+    }
+}
