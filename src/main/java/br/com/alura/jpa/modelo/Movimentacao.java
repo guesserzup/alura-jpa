67d356d4eb5d1641a613b0b91f113daf5c00a08f
Gabriel Guesser <gabriel.guesser@zup.com.br>
2021-08-16 10:38:37 -0300

    creating a enum and a class for types of bank transactions

---
+++ src/main/java/br/com/alura/jpa/modelo/Movimentacao.java
@@ -0,0 +1,60 @@
+package br.com.alura.jpa.modelo;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+public class Movimentacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal valor;
+
+    @Enumerated(EnumType.STRING)
+    private TipoMovimentacao tipoMovimentacao;
+
+    private LocalDateTime data;
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public TipoMovimentacao getTipoMovimentacao() {
+        return tipoMovimentacao;
+    }
+
+    public void setTipoMovimentacao(TipoMovimentacao tipoMovimentacao) {
+        this.tipoMovimentacao = tipoMovimentacao;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
--- src/main/java/br/com/alura/jpa/testes/TestandoEstados.java
+++ src/main/java/br/com/alura/jpa/testes/TestandoEstados.java
@@ -30,5 +30,4 @@ public class TestandoEstados {
 		
 		em.getTransaction().commit();
 	}
-
 }
