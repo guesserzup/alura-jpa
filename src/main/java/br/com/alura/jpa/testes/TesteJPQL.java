0d8b5cce998a2c62b79db96e47318081cfaef18e
Gabriel Guesser <gabriel.guesser@zup.com.br>
2021-08-16 15:37:55 -0300

    using TypedQuery to get rid of a warning due to the List type

--- src/main/java/br/com/alura/jpa/testes/TesteJPQL.java
+++ src/main/java/br/com/alura/jpa/testes/TesteJPQL.java
@@ -3,10 +3,7 @@ package br.com.alura.jpa.testes;
 import br.com.alura.jpa.modelo.Conta;
 import br.com.alura.jpa.modelo.Movimentacao;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.Query;
+import javax.persistence.*;
 import java.util.List;
 
 public class TesteJPQL {
@@ -18,14 +15,15 @@ public class TesteJPQL {
         Conta conta = new Conta();
         conta.setId(1L);
 
-        String jpql = "select m from Movimentacao m where m.conta.id = :pConta";
-        Query query = em.createQuery(jpql);
+        String jpql = "select m from Movimentacao m where m.conta.id = :pConta order by m.valor DESC";
+        TypedQuery<Movimentacao> query = em.createQuery(jpql, Movimentacao.class);
         query.setParameter("pConta", conta.getId());
 
         List<Movimentacao> resultList = query.getResultList();
 
         for (Movimentacao movimentacao : resultList) {
             System.out.println("Descrição: " + movimentacao.getDescricao());
+            System.out.println("Valor: " + movimentacao.getValor());
             System.out.println("Tipo: " + movimentacao.getTipoMovimentacao());
         }
     }
