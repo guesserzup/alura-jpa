d5e9255512d18da10c63699e10e4adea36711f36
Gabriel Guesser <gabriel.guesser@zup.com.br>
2021-08-16 09:04:28 -0300

    creating a stub class just to test out persisting a object in the db

--- src/main/java/br/com/alura/jpa/modelo/Conta.java
+++ src/main/java/br/com/alura/jpa/modelo/Conta.java
@@ -1,13 +1,17 @@
 package br.com.alura.jpa.modelo;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Conta {
 	
 	@Id
+	@GeneratedValue
 	private Long id;
+	
 	private Integer agencia;
 	private Integer numero;
 	private String titular;
