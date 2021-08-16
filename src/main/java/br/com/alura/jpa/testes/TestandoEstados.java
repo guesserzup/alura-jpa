97637a403f4c1283a951bc4ff45e2f238c78cd0f
Gabriel Guesser <gabriel.guesser@zup.com.br>
2021-08-16 10:31:01 -0300

    diff between transient, managed and removed

--- src/main/java/br/com/alura/jpa/testes/TestandoEstados.java
+++ src/main/java/br/com/alura/jpa/testes/TestandoEstados.java
@@ -1,5 +1,9 @@
 package br.com.alura.jpa.testes;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import br.com.alura.jpa.modelo.Conta;
 
 public class TestandoEstados {
@@ -8,11 +12,23 @@ public class TestandoEstados {
 		
 		Conta conta = new Conta();
 		
-		//Transient
+		// Transient
 		conta.setTitular("Almiro");
 		conta.setAgencia(12353);
 		conta.setNumero(542342);
 		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin();
+		
+		// Transient -> Managed
+		em.persist(conta);
+		
+		// Managed -> Removed
+		em.remove(conta);
+		
+		em.getTransaction().commit();
 	}
 
 }
