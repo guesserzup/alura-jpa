021e91723867180e770d52e9335f51b0f3270d6c
Gabriel Guesser <gabriel.guesser@zup.com.br>
2021-08-16 11:38:07 -0300

    testing the relationship between Movimentacao -> Conta

--- src/main/java/br/com/alura/jpa/testes/TestaRelacionamento.java
+++ src/main/java/br/com/alura/jpa/testes/TestaRelacionamento.java
@@ -1,5 +1,40 @@
 package br.com.alura.jpa.testes;
 
+import br.com.alura.jpa.modelo.Conta;
+import br.com.alura.jpa.modelo.Movimentacao;
+import br.com.alura.jpa.modelo.TipoMovimentacao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class TestaRelacionamento {
-    
+    public static void main(String[] args) {
+
+        Conta conta = new Conta();
+        conta.setAgencia(24234);
+        conta.setNumero(59854);
+        conta.setSaldo(300.0);
+        conta.setTitular("Albino");
+
+        Movimentacao movimentacao = new Movimentacao();
+        movimentacao.setData(LocalDateTime.now());
+        movimentacao.setDescricao("Churrascaria");
+        movimentacao.setValor(new BigDecimal(200.0));
+        movimentacao.setTipoMovimentacao(TipoMovimentacao.ENTRADA);
+        movimentacao.setConta(conta);
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        em.persist(conta);
+        em.persist(movimentacao);
+
+        em.getTransaction().commit();
+        em.clear();
+    }
 }
